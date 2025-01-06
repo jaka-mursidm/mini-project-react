cd3b1b698c79531de4af04e9efcff59c52c45692
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-06 21:40:33 +0700

    Membuat style button custom

---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+        <button className='bg-indigo-600 py-2 px-6 hover:bg-indigo-500 duration-100 text-white rounded-lg'>
+            <span>Button</span>
+        </button>
+    </>
+  )
+}
+
+export default Button
