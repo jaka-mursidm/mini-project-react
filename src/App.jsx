5b2954ee61cb20a413b2659feb8121b542aa4e94
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-06 21:19:41 +0700

    Membuat komponen button

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 
 import React from 'react'
+import Button from './components/button';
 const App = () => {
-  const name = 'Jaka'; 
-  const x = 10;
-  const y =  5;
-  const buah = ['Pisang','Jeruk','Semangka','Apel','Mangga'];
-  const login = true;
+ 
   return (
     <>
-      <h1 className='text-2xl'>Hello World!, My Name is {name}</h1>
-      <p className='text-slate-500'>Learn React</p>
-      <p> Hasil dari x + y adalah {x + y}</p>
-
-      <h3 className='text-xl mt-3'>List buah - buahan :</h3>
-      <ul>
-        {buah.map((buah,index)=>(
-          <li key={index}>{buah}</li>
-        ))}
-      </ul>
-
-      <h3 className='text-xl mt-3'>{login ? 'Selamat datang member!' : 'Selamat datang tamu!'}</h3>
+      <Button/>
     </>
   
   );
