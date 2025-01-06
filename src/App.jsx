fde847099a38f4a15593ca23eddc45473fe56f3d
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-06 20:32:47 +0700

    Belajar mengenal JSX dengan membuat variabel JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
 import React from 'react'
 const App = () => {
-  
+  const name = 'Jaka'; 
+  const x = 10;
+  const y =  5;
+  const buah = ['Pisang','Jeruk','Semangka','Apel','Mangga'];
   return (
     <>
-      <h1 className='text-2xl'>Hello World!, My Name is Jaka</h1>
-      <div className='text-slate-300'>Learn React</div>
+      <h1 className='text-2xl'>Hello World!, My Name is {name}</h1>
+      <p className='text-slate-500'>Learn React</p>
+      <p> Hasil dari x + y adalah {x + y}</p>
+
+      <h3 className='text-xl mt-3'>List buah - buahan :</h3>
+      <ul>
+        {buah.map((buah,index)=>(
+          <li key={index}>{buah}</li>
+        ))}
+      </ul>
     </>
   
   );
