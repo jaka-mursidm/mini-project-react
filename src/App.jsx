55d700f517ee4032da261928df4fa3c8341956a7
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-06 20:16:41 +0700

    Implementasi Tailwindcss dengan menambahkan kelass di elemen html

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const App = () => {
   
   return (
- 
-  <h1>Hello World!</h1>
+    <>
+      <h1 className='text-2xl'>Hello World!, My Name is Jaka</h1>
+      <div className='text-slate-300'>Learn React</div>
+    </>
   
   );
 }
