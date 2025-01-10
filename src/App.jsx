71cd5e1f1c0ea50bdf236dc1df5633ff19af5e92
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-10 16:05:57 +0700

    Membuat home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import Button from './components/button';
+import Home from './components/home';
 const App = () => {
  
   return (
     <>
-      <Button/>
+      <Home/>
     </>
   
   );
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <>
-        <button className='bg-indigo-600 py-2 px-6 hover:bg-indigo-500 duration-100 text-white rounded-lg'>
+        <button className='bg-opacity-35 bg-white py-2 px-6 hover:bg-indigo-500 duration-100 text-white rounded-lg'>
             <span>Button</span>
         </button>
     </>
