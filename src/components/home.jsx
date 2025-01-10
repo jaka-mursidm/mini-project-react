2e57355b0081ecb60461d6e4ffe5b8bbe786a5f2
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-10 16:06:33 +0700

    Membuat komponen home

---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './button'
+const home = () => {
+  return (
+    <>
+    <div className='min-h-80 bg-gradient-to-l from-indigo-400 to-indigo-800 text-white flex justify-center items-center'>
+        <div className='text-center'>
+        <h1 className='text-3xl'>Dapatkan Pekerjaan</h1>
+            <Button/>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default home
