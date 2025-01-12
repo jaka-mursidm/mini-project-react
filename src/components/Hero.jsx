76986441c40595f4829a01e49be74d8e01bc4a76
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-12 21:06:31 +0700

    Mengubah warna navbar dan hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className='min-h-80 bg-gradient-to-l from-indigo-400 to-indigo-800 text-white flex justify-center items-center'>
+    <div className='min-h-80 bg-gradient-to-l from-blue-400 to-blue-800 text-white flex justify-center items-center'>
         <div className='text-center'>
             <h1 className='text-4xl font-bold mb-2'>Semua lowongan ada disini</h1>
             <p>Jelajahi perusahaan dan menjadi profesional</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
 
-    <nav className='bg-gradient-to-l from-indigo-400 to-indigo-800 w-full'>
+    <nav className='bg-gradient-to-l from-blue-400 to-blue-800 w-full'>
         <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
             <div className='flex h-16 items-center justify-between text-white font-medium'>
                 <a href='#'><img src='' alt='' />Belajar React</a>
                 <div>
-                    <a href='#Beranda' className='hover:text-indigo-500 duration-75'>Beranda</a>
-                    <a href='#Perusahaan' className='hover:text-indigo-500 mx-4 duration-75'>Perusahaan</a>
+                    <a href='#Beranda' className='hover:text-blue-500 duration-75'>Beranda</a>
+                    <a href='#Perusahaan' className='hover:text-blue-500 mx-4 duration-75'>Perusahaan</a>
                     <a href="#">Tambah pekerjaan</a>
                 </div>
 
