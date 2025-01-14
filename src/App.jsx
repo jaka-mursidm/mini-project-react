c67fa8a73cbc6087fe3c666fc1872abae2d480d7
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-14 21:58:45 +0700

    Membuat konten dan style CompanyList

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@
 import React from 'react'
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Card from './components/Card';
+import CompanyList from './components/CompanyList';
+
 const App = () => {
  
   return (
     <>
       <Navbar/>
       <Hero title ='Semua lowongan ada disini' subtitle='Jelajahi perusahaan dan menjadi profesional'/>
-      <Card>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repudiandae illum debitis, aperiam inventore totam vero placeat officia. Maxime sapiente magnam quidem libero repellendus nemo fugiat tempora nobis, natus quo.</p>
-      </Card>
+      <CompanyList/>
     </>
   
   );
--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import Card from "./Card";
+import Button from "./Button";
 const CompanyList = () => {
   return (
-    <div>CompanyList</div>
-  )
-}
+    <>
+      <div className="max-w-7xl mx-auto mt-8">
+        <div className="grid grid-cols-4 gap-3">
+          <Card p="p-6">
+            <div className="flex flex-col align-center justify-center">
+              <div className="bg-slate-200 h-20 w-20"></div>
+              <h3 className="font-medium text-xl my-3">PT. A</h3>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-between mt-6">
+                <Button />
+                <span className="h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  20 Pekerjaan
+                </span>
+              </div>
+            </div>
+          </Card>
+          <Card p="p-6">
+            <div className="flex flex-col align-center justify-center">
+              <div className="bg-slate-200 h-20 w-20"></div>
+              <h3 className="font-medium text-xl my-3">PT. B</h3>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-between mt-6">
+                <Button />
+                <span className="h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  20 Pekerjaan
+                </span>
+              </div>
+            </div>
+          </Card>
+          <Card p="p-6">
+            <div className="flex flex-col align-center justify-center">
+              <div className="bg-slate-200 h-20 w-20"></div>
+              <h3 className="font-medium text-xl my-3">PT. C</h3>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-between mt-6">
+                <Button />
+                <span className="h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  20 Pekerjaan
+                </span>
+              </div>
+            </div>
+          </Card>
+          <Card p="p-6">
+            <div className="flex flex-col align-center justify-center">
+              <div className="bg-slate-200 h-20 w-20"></div>
+              <h3 className="font-medium text-xl my-3">PT. D</h3>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-between mt-6">
+                <Button />
+                <span className="h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  20 Pekerjaan
+                </span>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CompanyList
+export default CompanyList;
