7c9fb257ff78ced11aa536422a2a6db28f053601
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-14 21:34:06 +0700

    Membuat props style card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import React from 'react'
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 const App = () => {
  
   return (
     <>
       <Navbar/>
       <Hero title ='Semua lowongan ada disini' subtitle='Jelajahi perusahaan dan menjadi profesional'/>
+      <Card>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repudiandae illum debitis, aperiam inventore totam vero placeat officia. Maxime sapiente magnam quidem libero repellendus nemo fugiat tempora nobis, natus quo.</p>
+      </Card>
     </>
   
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Card = ({children}) => {
+const Card = ({children,p ='p-4'}) => {
   return (
-    <div className='border-2 border-slate-300 shadow-md p-4'>
+    <div className={`border-[1px] border-slate-300 shadow-sm rounded-2xl ${p}`}>
         {children}
     </div>
   )
