1e4a7489e639d974edd78927bd98121329f94aff
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-17 18:05:45 +0700

    Styling footer dan link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,21 +4,36 @@ const Footer = () => {
   return (
     <footer className="bg-black py-4 text-white">
       <div className="max-w-7xl mx-auto">
-        <h3 className="text-xl">Get In Touch</h3>
-        <div>
-          <a href="https://github.com/jaka-mursidm" target="_blank">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="https://websitepribadi.com" target="_blank">
-            <i class="fas fa-globe"></i>
-          </a>
-          <a href="mailto:email@domain.com">
-            <i class="fas fa-envelope"></i>
-          </a>
+        <div className="text-center">
+          <div className="text-2xl flex gap-2 justify-center mb-3">
+            <a
+              href="https://github.com/jaka-mursidm"
+              className="group/icon "
+              target="_blank"
+              title="Github"
+            >
+              <i class="fa-brands fa-github ease-out duration-100 group-hover/icon:scale-110"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/jaka.mursidm/"
+              title="Instagram"
+              className="group/icon"
+              target="_blank"
+            >
+              <i class="fa-brands fa-instagram ease-out duration-100 group-hover/icon:scale-110"></i>
+            </a>
+            <a
+              href="mailto:jaka.mursidm@gmail.com"
+              className="group/icon"
+              title="Gmail"
+            >
+              <i class="fa-solid fa-envelope ease-out duration-100 group-hover/icon:scale-110"></i>
+            </a>
+          </div>
+          <p className="text-xs">
+            &copy; 2025 Jaka Mursid M. - All Rights Reserved.
+          </p>
         </div>
-        <p className="text-xs">
-          &copy; 2025 Jaka Mursid M. - All Rights Reserved.
-        </p>
       </div>
     </footer>
   );
