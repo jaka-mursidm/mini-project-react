64138e2ffd3085f2e04a44ca26f2a8bae892ae3f
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-17 17:32:22 +0700

    Membuat konten JobList

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Card = ({ children, p = "p-4" }) => {
+const Card = ({ style, children, p = "p-4" }) => {
   return (
-    <div className={`border-[1px] border-slate-300 shadow-sm rounded-2xl ${p}`}>
+    <div
+      className={`border-[2px] border-slate-300 shadow-sm rounded-2xl ${style} ${p}`}
+    >
       {children}
     </div>
   );
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,10 +1,133 @@
 import Card from "./Card";
-
 const JobList = () => {
   return (
-    <Card>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, nulla.
-    </Card>
+    <div className="max-w-7xl mx-auto my-12 ">
+      <h2 className="text-2xl font-medium mb-4">
+        Telusuri keahlian dan minat anda
+      </h2>
+      <div className="grid grid-cols-3 gap-3">
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">Fullstack Developer</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-blue-200 text-blue-800 text-xs rounded-lg p-2 m-0">
+                  Full time
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Developer
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">Back-end Developer</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-indigo-200 text-indigo-800 text-xs rounded-lg p-2 m-0">
+                  Kontrak
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Developer
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">Front-end Developer</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-orange-200 text-orange-800 text-xs rounded-lg p-2 m-0">
+                  Remote
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Developer
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">Fullstack Developer</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-blue-200 text-blue-800 text-xs rounded-lg p-2 m-0">
+                  Full time
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Developer
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">Quality Assurance Tester</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-indigo-200 text-indigo-800 text-xs rounded-lg p-2 m-0">
+                  Kontrak
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Tester
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+        <a href="#" className="group/card">
+          <Card p="p-4" style="group-hover/card:border-blue-700">
+            <div className="flex flex-col align-center justify-center">
+              <h3 className="font-medium text-xl">UI/UX Developer</h3>
+              <h5 className="mb-3 mt-1">PT. ABC</h5>
+              <p className="text-sm text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                repudiandae impedit deleniti?
+              </p>
+              <div className="flex justify-end gap-2">
+                <span className="w-fit mt-4 h-fit bg-orange-200 text-orange-800 text-xs rounded-lg p-2 m-0">
+                  Remote
+                </span>
+                <span className="w-fit mt-4 h-fit bg-slate-200 text-xs rounded-lg p-2 m-0">
+                  Web Design
+                </span>
+              </div>
+            </div>
+          </Card>
+        </a>
+      </div>
+    </div>
   );
 };
 
