e7ba2c2b308181231b7820bbab32045432d10117
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-18 21:18:16 +0700

    Konfigurasi route react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import CompanyList from "./components/CompanyList";
 import JobList from "./components/JobList";
 import Footer from "./components/Footer";
-
-const App = () => {
-  return (
-    <>
-      <Navbar />
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <Hero
         title="Semua lowongan ada disini"
         subtitle="Jelajahi perusahaan dan menjadi profesional"
       />
+    ),
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+      {/* <Navbar />
+      
       <CompanyList />
       <JobList />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
