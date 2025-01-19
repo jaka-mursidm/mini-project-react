68208477fb47edac996c26d5649a941e19a32e33
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-19 15:55:59 +0700

    Update style button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Button = ({
   textSize = "text-md",
   bg = "bg-blue-600 hover:bg-blue-500",
+  border = "border-2 border-white hover:border-black",
+
   children,
 }) => {
   return (
     <>
       <button
-        className={`${textSize} font-medium py-2 px-6 ${bg} duration-100 text-white rounded-full`}
+        className={`${textSize}  font-medium py-2 px-6 ${bg}  ${border}  duration-100 text-white rounded-full`}
       >
         {children}
       </button>
