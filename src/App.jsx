2261bf9fa4a0fa2466eb7a5a87bd397c6a54c2c5
Jaka Mursid <jaka.mursidm@gmail.com>
2025-01-19 15:04:14 +0700

    Membuat CompanyPage dan integrasi link component route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
-
+import CompanyPage from "./pages/CompanyPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
+      { path: "perusahaan", element: <CompanyPage /> },
     ],
   },
 ]);
